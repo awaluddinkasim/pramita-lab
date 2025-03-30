9fa280b94e674b388ed8d4074ea3917b09fa82ca
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2025-03-30 22:49:04 +0800

    update view

--- resources/views/pages/admin/order-detail.blade.php
+++ resources/views/pages/admin/order-detail.blade.php
@@ -58,12 +58,12 @@
                         </div>
                     @endif
                 @else
-                    <div class="mb3">
+                    <div class="mb-5">
                         Masih menunggu pengiriman
                     </div>
 
                     <button onclick="location.href='{{ route('admin.order.edit', $order->id) }}'"
-                        class="m-1 ms-0 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
+                        class="ms-0 py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
                         Edit
                     </button>
                 @endif
